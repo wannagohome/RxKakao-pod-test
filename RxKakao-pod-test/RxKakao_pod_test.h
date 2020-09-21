95abc86ce1ce875170cab462d6f9fb0831958231
pete <iwwuf7@gmail.com>
2020-09-21 17:45:32 +0900

    0.2.0

---
+++ RxKakao-pod-test/RxKakao_pod_test.h
@@ -0,0 +1,19 @@
+//
+//  RxKakao_pod_test.h
+//  RxKakao-pod-test
+//
+//  Created by jinho jang on 2020/09/21.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxKakao_pod_test.
+FOUNDATION_EXPORT double RxKakao_pod_testVersionNumber;
+
+//! Project version string for RxKakao_pod_test.
+FOUNDATION_EXPORT const unsigned char RxKakao_pod_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxKakao_pod_test/PublicHeader.h>
+
+
